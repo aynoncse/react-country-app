2473e3ac32f89717250636d2f331ba1d7cc2b8bd
aynoncse <aynoncse@gmail.com>
2024-09-18 02:24:32 +0600

    Data fetching

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import React, { useState, useEffet, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [countries, setCountries] = useState([]);
+  const url = "https://restcountries.com/v3.1/all";
+
+  const fetchData = async (url) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setCountries(data);
+      setIsLoading(false);
+      setError(null);
+      console.log(data);
+
+    } catch (error) {
+      setIsLoading(false);
+      setError(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData(url);
+  }, []);
+
   return (
     <div>
 
